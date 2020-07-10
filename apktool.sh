04ff642f29eac9730e2a07bd27f8b5c3d0a3bfb9
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2020-07-10 12:00:16 +0530

    Add files via upload

---
+++ apktool.sh
@@ -0,0 +1,46 @@
+echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";
+apt update && apt upgrade
+pip3 install gem
+gem install lolcat
+pkg install wget curl -y
+clear 
+banner ( ) { 
+                  random
+                  toilet -F border -f mono12  "Apktool"  | lolcat
+                  printf ""
+                  printf "\e[1;34m[*] \e[1;96mcreated by \e[1;33m Raj Aryan ( @h4ck3r_0 )\e[0m"
+                  }
+                  
+                  wr  ( )  {
+                               printf "\033[1;91m Invalid input!!!\n"
+                               selection
+                               }
+                               Apktool() {
+                                                 cd files
+                                                 apt install ./apktool.deb
+                                                 }
+                                                 Java() {
+                                                               cd files
+                                                               bash java.sh
+                                                               }
+                                                               
+                                 selection () {
+                                            cd ~/apktool-termux
+                                            echo -e -n "\e[1;96m Apktool\e[1;96m Termux : \e[0m"
+                                            read a
+                                            case $a in
+                                            1) Apktool ;;
+                                            2) Java ;;
+                                            3) exit ;;
+                                            *) wr ;;
+                                            esac
+                                            }
+                                 
+                  menu () {
+                                  banner
+                                  printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Apktool \n"
+                                  printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Java \n"
+                                  printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;92m Exit\n\n\n"
+                                  selection
+                                  }
+                  menu
