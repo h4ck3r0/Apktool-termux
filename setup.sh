1693edecf446cbb02037d23c83b409e8bf2d3828
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2020-07-10 12:22:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ apt update && apt upgrade
 pip3 install gem
 gem install lolcat
 pkg install wget curl -y
+chmod +x setup.sh
+chmod +x apktool.sh
+chmod +x /files/java.sh
 echo -e "\e[1;34m[√] \e[96mNow run bash apktool.sh \e[0m"
