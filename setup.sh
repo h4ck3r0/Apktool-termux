74ab6acedb04895d33e58a2a9e0b03d01094ef08
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-11 10:05:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
-echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";
+clear
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED} ███████ ▓█████▄▄▄█████▓ █    ██  ██▓███  " 
+echo -e "${RED}▒██    ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒" 
+echo -e "${RED}░ ▓██▄   ▒███  ▒ ▓██░ ▒░▓██  ▒██░▓██░ ██▓▒" 
+echo -e "${RED}  ▒   ██▒▒▓█  ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒" 
+echo -e "${RED}▒██████▒▒░▒████▒ ▒██▒ ░ ▒▒█████▓ ▒██▒ ░  ░" 
+echo -e "${RED}▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░" 
+echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ ░▒ ░     " 
+echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░       " 
+echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
+                                          
+echo  ""                                          
+echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";                                       
 apt update && apt upgrade
 pkg install toilet
 pip3 install gem
