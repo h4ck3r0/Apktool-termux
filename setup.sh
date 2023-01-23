33ea855a354b5e3b47b6e6b003c15b5533a18651
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2023-01-23 17:44:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░      
 echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
                                           
 echo  ""                                          
-echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";                                       
+echo -e "\e[1;34m[*] \e[32mInstalling Packages....\e[0m";                                       
                                           
                                                                                                 
 apt update && apt upgrade -y
